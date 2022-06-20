499016012bf35989b74495173759d0e81677a635
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-20 18:48:47 +0900

    fix link to portfolio in about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,7 +7,7 @@ function AboutPage() {
     <Card>
       <div className="about">
         <h1>About This Project</h1>
-        <p className="">Coded by <Link to={{ pathname: "https://tirsop.com" }} target="_blank">Tirso Pascual</Link>.</p>
+        <p className="">Coded by <a href="https://tirsop.com" target="_blank">Tirso Pascual</a>.</p>
         <p className="">This is a React app to leave feedback for a product or service. It is build with React using hooks (useState, useContext, useEffect), it has styling components and uses the React router.</p>
         <Link to="/">Back to home page</Link>
       </div>
